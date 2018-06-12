12c15f91fc033d72ac293f939de8a054daf512f9
Iris The Fox <iris-miz@yandex.ru>
2018-06-12 22:31:08 +0300

    add columns, change link

--- app/parsscrap/parsscrap.rb
+++ app/parsscrap/parsscrap.rb
@@ -28,34 +28,40 @@ end
 end
 =end
 
+Game.destroy_all
 
 games = []
 
 doc.css('tr').each do |game|
-if game.css("td")[0]===nil
-  puts "ololololololo"
-else
-  name_g = game.css("td")[0].text
-  developer = game.css("td")[1].text
-  release_date = game.css("td")[2].text
-  genre = game.css("td")[3].text
-  description = game.css("td")[4].text
-  trailer = game.css("td")[5].text
-games.push(
-  name_g: name_g,
-  developer: developer,
-  release_date: release_date,
-  genre: genre,
-  description: description,
-  trailer: trailer)
-
+  if game.css("td")[0]===nil
+    puts "ololololololo"
+  else
+    name_g = game.css("td")[0].text
+    developer = game.css("td")[1].text
+    release_date = game.css("td")[2].text
+    genre = game.css("td")[3].text
+    description = game.css("td")[4].text
+  
+    ff = game.css("td")[5].css("a")[0]["id"]
+    trailer = "https://www.youtube.com/watch?v=#{ff}"
+ 
+    games.push(
+      name_g: name_g,
+      developer: developer,
+      release_date: release_date,
+      genre: genre,
+      description: description,
+      trailer: trailer)
+  end
 end
-end
-
+puts "___"
 puts games[0]
 puts games[0][:name_g]
+puts games[0][:trailer]
 
+
+=begin
 games.each do |i|
-  Game.create(name_g:i[:name_g], developer:i[:developer], release_date:i[:release_date], genre:i[:genre], description:i[:description], trailer:i[:trailer], platform:"dunno", wanna_play:true)
+  Game.create(name_g:i[:name_g], developer:i[:developer], release_date:i[:release_date], genre:i[:genre], description:i[:description], trailer:i[:trailer], platform:"PlayStation 4", wanna_play:true)
 end
-
+=end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180612140059) do
+ActiveRecord::Schema.define(version: 20180612192847) do
 
   create_table "books", force: :cascade do |t|
     t.string   "autor"
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(version: 20180612140059) do
     t.string   "trailer"
     t.string   "platform"
     t.boolean  "wanna_play"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "year",         default: 2018
   end
 
   create_table "sommodels", force: :cascade do |t|
